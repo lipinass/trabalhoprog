e026f1d48fda9d86822e09810a97c30fad3fe2f0
DuMattoss <odumattos@gmail.com>
2025-01-27 15:22:48 -0300

    Update Trabalho Programação.c

--- Trabalho Programação.c
+++ Trabalho Programação.c
@@ -1,5 +1,4 @@
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,11 +11,23 @@ struct Encomenda {
     char Transportadora[50];
 };
 
-int main(){
-ffffffffffffff
 
-    
+void NovaEncomenda(Encomenda encomenda){
+    File *arquivo = fopen("encomendas.txt", "a");
+        if(arquivo==Null){
+            printf("Erro ao abrir o arquivo. \n");
+            return;
+        }
+        
+        fprint(arquivo, "%s,%s,%s,%s,%s\n", encomenda.Codigo, encomenda.Destinatario, encomenda.Destino, encomenda.DatadeEnvio, encomenda.Transportadora);
+        fclose(arquivo);
+        printf("Pacote registrado!.\n");
+}
+
+
+int main(){
+
+
+
     return 0;
 }  
-
-
