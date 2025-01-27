e9721a7c36babe8dcba7c6b3ce0063988d89fc33
DuMattoss <odumattos@gmail.com>
2025-01-27 15:02:53 -0300

    Add files via upload

---
+++ Trabalho Programação.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Encomenda {
+    char Codigo[9];
+    char Destinatario[50];
+    char Destino[50];
+    char DatadeEnvio[11];
+    char Transportadora[50];
+};
+
+int main(){
+
+    return 0;
+}  
+
+
